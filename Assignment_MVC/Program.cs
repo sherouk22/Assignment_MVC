64f5d223509b7b1769bd5a051f6c7a277b6c9b70
sherouk22 <sheroukmohy558@gmail.com>
2025-03-20 20:14:00 +0200

    Add project files.

---
+++ Assignment_MVC/Program.cs
@@ -0,0 +1,15 @@
+namespace Assignment_MVC
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/Home", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
