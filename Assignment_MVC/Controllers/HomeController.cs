560e6c5f95f20e22202d0fb9ea73c25424a384ad
sherouk22 <sheroukmohy558@gmail.com>
2025-03-21 04:59:50 +0200

    Assignment02_MVC

---
+++ Assignment_MVC/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        //public ActionResult Index()
+        //{
+        //    return Content( "Hello From Index Action");
+
+        //}
+
+        //public ActionResult AboutUs()
+        //{
+        //    return Redirect("https://github.com/sherouk22/Assignment_MVC/tree/Assignment02_MVC");
+
+
+        //}
+
+        public IActionResult Index()
+        {
+            return View();
+
+        }
+
+        public IActionResult AboutUs()
+        {
+            return View();
+
+        }
+
+        public IActionResult ContactUs()
+        {
+            return View();
+
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+
+        }
+
+    }
+}
--- Assignment_MVC/Program.cs
+++ Assignment_MVC/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Http;
+
 namespace Assignment_MVC
 {
     public class Program
@@ -5,24 +7,76 @@ namespace Assignment_MVC
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllersWithViews();
             var app = builder.Build();
 
             // app.MapGet("/Home", () => "Hello World!");
 
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/Home", async context =>
-                {
-                    await context.Response.WriteAsync("You Are At Home Page.");
-                });
+            #region MyRegion
 
-                endpoints.MapPost("/Product", async context =>
-                {
-                    await context.Response.WriteAsync("You Are At Product Page.");
-                });
-            });
+            ////app.Use(async (context, next) =>
+            ////{
+            ////    Endpoint endpoint = context.GetEndpoint();
+
+            ////    if (endpoint is not null)
+            ////        await context.Response.WriteAsync("Your Requested Page Not Found.");
+
+
+            ////    await next();
+
+            ////});
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/Home", async context =>
+            //    {
+            //        await context.Response.WriteAsync("You Are At Home Page.");
+            //    });
+
+            //    endpoints.MapGet("/Product", async context =>
+            //    {
+            //        await context.Response.WriteAsync("You Are At Product Page.");
+            //    });
+
+            //    endpoints.MapGet("/Product/{id:int?}", async context =>
+            //    {
+            //        var idData = context.Request.RouteValues["id"];
+
+            //        if (idData is not null)
+            //        {
+            //            int id = Convert.ToInt32(context.Request.RouteValues["id"]);
+            //            await context.Response.WriteAsync($"Your Requested Product with id => {id} ");
+            //        }
+
+
+            //    });
+
+            //    endpoints.MapGet("/Books/{id}/{auther:alpha:minlength(3):maxlength(10)}", async context =>
+            //    {
+            //        int id = Convert.ToInt32(context.Request.RouteValues["id"]);
+            //        string auther = context.Request.RouteValues["auther"].ToString();
+            //        await context.Response.WriteAsync($"Your Requested Product with id => {id} Author => {auther} ");
+            //    });
+            //});
+
+            ////app.Run(async (HttpContext) =>
+            ////{
+            ////    await HttpContext.Response.WriteAsync("Your Requested Page Not Found.");
+
+            ////}); 
+            #endregion
+
+            app.MapAreaControllerRoute(
+
+                name:"default",
+                areaName:"default",
+                pattern:"/{Controller=Home}/{Action=Index}",
+                defaults: new { Controller = "Home" , Action ="Index"}
+
+            );
 
             app.Run();
         }
